be259048a15e970ccba481f86b716a02844ce5dc
jokamjohn <johnkagga@gmail.com>
2015-04-24 07:29:18 +0300

    Add message

--- helloworld_api.py
+++ helloworld_api.py
@@ -10,3 +10,15 @@ from protorpc import message_types
 from protorpc import remote
 
 # All of the imports shown are required for an Endpoints API backend.
+
+package = "hello"
+
+class Greeting(messages.Message):
+    """Greeting that stores a message."""
+message = messages.StringField(1)
+
+class GreetingCollection(messages.Message):
+    """Collection of Greetings."""
+items = messages.MessageField(Greeting, 1, repeated=True)
+
+STORED_GREETING = GreetingCollection(items[Greeting(message='hello world!'), Greeting(message='goodbye world!')])
