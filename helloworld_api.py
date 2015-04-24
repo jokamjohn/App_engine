8c09a2ec54f5e47bc19911224d4d253811a77c69
jokamjohn <johnkagga@gmail.com>
2015-04-24 07:03:13 +0300

    Added helloworld_api.py

---
+++ helloworld_api.py
@@ -0,0 +1,12 @@
+"""Hello World API implemented using Google Cloud Endpoints.
+
+Defined here are the ProtoRPC messages needed to define Schemas for methods
+as well as those methods defined in an API.
+"""
+
+import endpoints
+from protorpc import messages
+from protorpc import message_types
+from protorpc import remote
+
+# All of the imports shown are required for an Endpoints API backend.
